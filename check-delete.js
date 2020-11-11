c67ad9c7e4cfb40eef2d4ed08effa4c89f40f9d9
Traky <dengk7@mcmaster.ca>
2020-11-10 23:49:13 -0500

    updated functions & animations for check and delete

--- check-delete.js
+++ check-delete.js
@@ -3,11 +3,15 @@
 const listArea = document.querySelector(".todo-list");
 
 listArea.addEventListener("click", function (e) {
-    const item = e.target;
-    if (item.classList[0] == "button-checked") {
-        // item.parentElement.classList.toggle("completed");
-        item.parentElement.remove();
+    const click = e.target;
+    if (click.classList[0] == "button-checked") {
+        click.parentElement.classList.toggle("completed");
     }
-    else if (item.classList[0] == "button-delete") {
+    else if (click.classList[0] == "button-delete") {
+        const item = click.parentElement;
+        item.classList.add("fall");
+        item.addEventListener("transitionend", function () {
+            item.remove()
+        })
     }
 })
