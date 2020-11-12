93bfe6edafb7855655b59b668a351f54b335baaf
Traky <dengk7@mcmaster.ca>
2020-11-12 17:08:02 -0500

    add tasks to local storage

---
+++ localStorage.js
@@ -0,0 +1,12 @@
+function saveToLocalStorage(todo) {
+    // checking local storage
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
